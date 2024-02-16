5cdecde72f58bb80ea234ca3d73e44d97f3ec398
gaplin <gaplinn@gmail.com>
2024-02-16 21:40:02 +0100

    Removed greeting line from solve.c

--- src/c/solve.c
+++ src/c/solve.c
@@ -6,7 +6,6 @@
 int main(int argc, char* argv[]) {
     reseed_rand(time(NULL));
     int puzzle[SUDOKU_NN][SUDOKU_NN];
-    printf("Provide %d numbers. For unknowns use 0.\n", SUDOKU_ALL);
 
     for(int i = 0; i < SUDOKU_NN; ++i) {
         for(int j = 0; j < SUDOKU_NN; ++j) {
